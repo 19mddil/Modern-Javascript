2053e8fdab1d48199a810e97e9457afbe2cf49d4
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-07-28 07:01:11 +0600

    a super use of function property and user:

--- important_codes/******ch5/closure/lexical.js
+++ important_codes/******ch5/closure/lexical.js
@@ -1,10 +1,42 @@
-function makeCounter() {
-    let count = 0;
-    return function counter() {
-        return count++;
+{
+    function makeCounter() {
+        let cnt = 0;
+        function counter() {
+            return cnt++;
+        }
+        //A super user of function property and closure
+        counter.set = value => cnt = value;
+        counter.increase = () => ++cnt;
+        counter.decrease = () => --cnt;
+        return counter;
     }
+    let c = makeCounter();///uffff now I understand
+    console.log(c());
+    console.log(c());
+    console.log(c());
+    console.log(c.set(10));
+    console.log(c.increase());
+    console.log(c.decrease());
 }
-let c = makeCounter();///uffff now I understand
-console.log(c());
-console.log(c());
-console.log(c());
+{
+    function makeCounter() {
+        let count = 0;
+        function counter() {
+            return counter.count++;
+        }
+        //A super user of function property and closure
+        counter.count = 0;
+        counter.set = value => counter.count = value;
+        counter.increase = () => ++counter.count;
+        counter.decrease = () => --counter.count;
+        return counter;
+    }
+
+    let c = makeCounter();///uffff now I understand
+    console.log(c());
+    console.log(c());
+    console.log(c());
+    console.log(c.set(10));
+    console.log(c.increase());
+    console.log(c.decrease());
+}
