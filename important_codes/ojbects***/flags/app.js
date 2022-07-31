01439e65588c7e6c8cb3fd2b50031d8c362121b2
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-07-31 09:15:48 +0600

    object falgs consumed

---
+++ important_codes/ojbects***/flags/app.js
@@ -0,0 +1,24 @@
+'use strict'
+let user = {
+    name: "John Wick",
+    profession: "assasin",
+    toString() {
+        return this.name + ' the ' + this.profession;
+    }
+};
+Object.defineProperties(user, {
+    name: { writable: false },
+    profession: { configurable: false },/*its now forever writable true and enurable true,not deletable*/
+    toString: { enumerable: false },
+});
+console.log(Object.keys(user));
+// user.profession = 'store keeper';
+console.log(user);
+// delete user.profession;
+delete user.name;
+console.log(user);
+Object.defineProperties(user, {
+    profession: { writable: false },
+})
+user.profession = 'store keeper';
+console.log(user);
