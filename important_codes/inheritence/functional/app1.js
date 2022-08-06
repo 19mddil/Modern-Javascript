c1e21e252bf3bb2fc0a136ea76c49e1f5ac29472
19mddilAspire <mddilshadul98@gmail.com>
2022-08-06 13:29:38 +0600

    fixed buggs

--- important_codes/inheritence/functional/app1.js
+++ important_codes/inheritence/functional/app1.js
@@ -1,58 +1,54 @@
-// {
-//     let animal = {
-//         eat(){
-//             console.log("It eats.");
-//         }
-//     }
-    
-//     function Cat(name){
-//         this.name = name;
-//         console.log(this.name);
-//     }
-    
-    
-//     console.log(typeof Cat.prototype);
-    
-//     console.log(Cat.prototype.constructor == Cat);
-//     Cat.prototype = animal;
-//     console.log(Cat.prototype.constructor == Cat);
-    
-//     console.log(Object.getOwnPropertyNames(Cat.prototype));
-    
-//     let cat1 = new Cat("Lara");
-//     cat1.eat();//it works
-    
-//     let cat2 = new cat1.constructor("Jasmin");// because the constructor is the function refers to Cat.
-//     // cat2.eat();//it doesn't
-// }
 {
-    let animal = {
-        eat(){
-            console.log("It eats.");
-        }
+    function Animal() {
+
     }
-    
-    function Cat(name){
-        
-        //Cat.prototype = animal;
+
+    let animal = new Animal();
+    animal.eat = () => {
+        console.log("it eats.");
+    };
+    animal.eat();
+
+    function Cat(name) {
         this.name = name;
         console.log(this.name);
     }
-    console.log(Cat.prototype.constructor == Cat);
-    // Cat.prototype = animal;
-    console.log(Cat.prototype.constructor);
+
+    console.log(Cat.prototype.constructor.name);
+    Cat.prototype = animal; // activated when new Cat("XXX") is called
+    console.log(Cat.prototype.constructor.name);
 
     let cat1 = new Cat("Lara");
-    cat1.__proto__ = animal;
     cat1.eat();//it works
     console.log(cat1.name);
-    
-    
-    let cat2 = new cat1.constructor("Jasmin");// because the constructor is the function refers to Cat.
-    cat2.__proto__ = animal;
+
+    console.log(cat1.constructor.name);
+    cat1.constructor = Cat;//** fixed by only this line*/
+    let cat2 = new cat1.constructor("Jasmin");// because the constructor is the function refers to Animal before.
+    console.log(cat2.name);// and shows undefined
     cat2.eat();//it doesn't
-    console.log(cat2.name);//shows undefined
-
+    /**
+     function User(name) {
+        this.name = name;
+    }
+    User.prototype = {}; // (*)let user = new User('John');
+    let user2 = new user.constructor('Pete');
+    console.log( user2.name ); // undefined
+    */
+    /**
+    1. First, it looks for constructor in user . Nothing.
+    2. Then it follows the prototype chain. The prototype of user is User.prototype and it also has nothing.
+    3. The value of User.prototype is a plain object {} , its prototype is Object.prototype . And there is Object.prototype.constructor == Object . So it is used instead of User.
+     */
 }
-
-
+{
+    /** A simple use of it */
+    function Cat(name) {
+        this.name = name;
+        console.log("hi,", name, "cat");
+    }
+    console.log(Cat.prototype.constructor.name);
+    let cat1 = new Cat("Lara");
+    let cat2 = new cat1.constructor("Jasmin");
+    /**it works perfect because Cat.prototype is Cat */
+}
---
+++ important_codes/inheritence/functional/task1.js
@@ -0,0 +1,9 @@
+function Rabbit() {
+
+}
+Rabbit.prototype = {
+    eats: true
+};
+let rabbit = new Rabbit();
+Rabbit.prototype.eats = false;
+console.log( rabbit.eats ); // false
