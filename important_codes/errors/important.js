2aace52d3334f3bff3781c455a507267f838f025
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-08-23 03:37:02 +0600

    function return is delayed by finally

---
+++ important_codes/errors/important.js
@@ -0,0 +1,10 @@
+function tester() {
+    try {
+        return 1;
+    } catch (e) {
+
+    } finally {
+        console.log("Hello, when I will be rich you will become my bitch");
+    }
+}
+console.log(tester());// interesting code
