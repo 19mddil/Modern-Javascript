435b9a34c57adadd942b1b66d07141ff199fb202
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2022-08-30 01:29:05 +0600

    throwing error from generators

--- important_codes/generator/app3.js
+++ important_codes/generator/app3.js
@@ -1,17 +1,31 @@
 function* gen() {
     let ask = yield "2 + 2?";
     console.log(ask);
-    let ask1 = yield "3 * 3?";
-    console.log(ask1);
-    let ask3 = yield "4 + 4?";
-    console.log(ask3);
+    try {
+        let ask1 = yield "3 * 3?";
+        // console.log(ask1);
+    } catch (err) {
+        console.log(err.message);
+    }
+    try {
+        let ask3 = yield "4 + 4?";
+        console.log(ask3);
+    } catch (err) {
+        console.log(err.message);
+    }
 }
-let genarator = gen();
-console.log(genarator.next().value);//"2 + 2?";
-//4
-console.log(genarator.next(4).value);//"3 * 3?";
-//9
-console.log(genarator.next(9).value);//"4 + 4?";
-//8
-console.log(genarator.next(8).done);//true;s
+try {
+
+    let genarator = gen();
+    console.log(genarator.next().value);//"2 + 2?";
+    //4
+    console.log(genarator.next(4).value);//"3 * 3?";
+    genarator.throw(new Error("Made up"));
+    //9
+    console.log(genarator.next().value);//"4 + 4?";
+    //8
+    console.log(genarator.next(8).done);//true;s
+} catch (err) {
+    console.log(err);
+}
 
